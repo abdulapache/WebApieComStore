4bb27312e99740efffd283a26d42e8f2282ff0b7
AR Rahman <rahmanmajeed783@gmail.com>
2023-07-06 17:30:49 +0500

    All Code of webApi

---
+++ eCommerceBuilder/Data/eCommerceBuilderContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using eCommerceBuilder.Models;
+
+namespace eCommerceBuilder.Data
+{
+    public class eCommerceBuilderContext : DbContext
+    {
+        public eCommerceBuilderContext (DbContextOptions<eCommerceBuilderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<eCommerceBuilder.Models.StoreRequestQue> StoreRequestQue { get; set; } = default!;
+
+        public DbSet<eCommerceBuilder.Models.WebsiteType>? WebsiteType { get; set; }
+    }
+}
